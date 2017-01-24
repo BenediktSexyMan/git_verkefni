cb138c3fd9a2bcd4fed2fee9bed34795497be772
BenediktSexyMan <benediktsi228@nemi.tskoli.is>
2017-01-24 09:06:31 +0000

    Gerði dæmi 1 í forritun

---
+++ git_forritun.py
@@ -0,0 +1,5 @@
+#Forritun Git Verkefni
+#Dæmi 1
+num1 = int(input("Gefðu mér tvær heiltölur!\n"))
+num2 = int(input())
+print("Tölurnar lagðar saman eru:", num1 + num2, "\nTölurnar margfaldaðar saman eru:", num1 * num2)
