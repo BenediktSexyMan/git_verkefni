218203917ad21b0d0578d690f61222b9192b26d6
BenediktSexyMan <benediktsi228@nemi.tskoli.is>
2017-01-24 09:16:28 +0000

    Gerði dæmi 2 í forritun og bætti valmynd

--- git_forritun.py
+++ git_forritun.py
@@ -1,5 +1,11 @@
 #Forritun Git Verkefni
-#Dæmi 1
-num1 = int(input("Gefðu mér tvær heiltölur!\n"))
-num2 = int(input())
-print("Tölurnar lagðar saman eru:", num1 + num2, "\nTölurnar margfaldaðar saman eru:", num1 * num2)
+val = input("Hvað viltu gera?\n1: 2 tölur?\n2: Nöfn?\n3:")
+while val != "4":
+    if val == "1":
+        num1 = int(input("Gefðu mér tvær heiltölur!\n"))
+        num2 = int(input())
+        print("Tölurnar lagðar saman eru:", num1 + num2, "\nTölurnar margfaldaðar saman eru:", num1 * num2)
+    if val == "2":
+        fornafn = input("Hvert er nafn þitt?\n")
+        eftirnafn = input("Hæ " + fornafn + "!\nHvert er eftirnafn þitt?\n")
+        print("Hæ", fornafn + " " + eftirnafn)
